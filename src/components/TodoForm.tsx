2af47d8c7f197641b2b669e45275e6ee1ac38017
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-09-24 18:01:24 -0300

    Adding new tasks

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,35 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { FiPlusCircle } from "react-icons/fi";
 import styles from './TodoForm.module.css';
 
-export function TodoForm() {
+interface TodoFormInterface {
+  createTask: (taskName: string) => void;
+}
+
+export function TodoForm({ createTask }: TodoFormInterface) {
+  const [task, setTask] = useState('');
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setTask(event.target.value);
+  }
+
+  function handleCreateTask(event: FormEvent) {
+    event.preventDefault();
+
+    createTask(task);
+    setTask('');
+  }
+
   return (
-    <form className={styles.form} autoComplete="off">
-        <input type="text" name="task" placeholder="Adicione uma nova tarefa" value="" readOnly />
+    <form onSubmit={handleCreateTask} className={styles.form} autoComplete="off">
+        <input
+          type="text"
+          name="task"
+          placeholder="Adicione uma nova tarefa"
+          value={task}
+          onChange={handleNewTaskChange}
+          required
+        />
         <button type="submit"><span>Criar</span> <FiPlusCircle size={16} /></button>
     </form>
   )
